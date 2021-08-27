2feed209066f95977d2ad439ef60cf253c3c8f4b
Zakhanugraha <82750740+Zakhanugraha@users.noreply.github.com>
2021-08-27 22:34:12 +0700

    Update Movie.js

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 //import data yang diambil
-import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
+// import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
 //import components
 import BreadCrumb from './BreadCrumb';
-import Grid from './Grid';
+// import Grid from './Grid';
 import Spinner from './Spinner';
 import MovieInfo from './MovieInfo';
 // import custom hooks dari useMovieFetch
 import { useMovieFetch } from '../hooks/useMovieFetch';
 //import image not found
-import NoImage from '../images/no_image.jpg';
+// import NoImage from '../images/no_image.jpg';
 const Movie = () => {
     const { movieId } = useParams();
 
