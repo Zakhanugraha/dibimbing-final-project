0caa72cba56d34164bcb15ee03c03d661ce45f88
Zakharia Nugraha <zakharia.nugraha21@gmail.com>
2021-08-29 20:25:32 +0700

    edit some files and delete helper.js

--- src/config.js
+++ src/config.js
@@ -1,8 +1,6 @@
-// Configuration for TMDB API
-// Read more about the API here: https://developers.themoviedb.org/
 
 // const API_URL = 'https://api.themoviedb.org/3/';
-const API_KEY = process.env.REACT_APP_API_KEY;
+// const API_KEY = process.env.REACT_APP_API_KEY;
 // const SEARCH_BASE_URL = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=`;
 // const POPULAR_BASE_URL = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US`;
 
@@ -13,9 +11,9 @@ const SEARCH_BASE_URL = `${API_URL}movies/search?key=`;
 const POPULAR_BASE_URL = `${API_URL}movies/popular`;
 
 // For login and voting
-const REQUEST_TOKEN_URL = `${API_URL}authentication/token/new?api_key=${API_KEY}`;
-const LOGIN_URL = `${API_URL}authentication/token/validate_with_login?api_key=${API_KEY}`;
-const SESSION_ID_URL = `${API_URL}authentication/session/new?api_key=${API_KEY}`;
+// const REQUEST_TOKEN_URL = `${API_URL}authentication/token/new?api_key=${API_KEY}`;
+// const LOGIN_URL = `${API_URL}authentication/token/validate_with_login?api_key=${API_KEY}`;
+// const SESSION_ID_URL = `${API_URL}authentication/session/new?api_key=${API_KEY}`;
 
 const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
 // Sizes: w300, w780, w1280, original
@@ -27,11 +25,11 @@ export {
   SEARCH_BASE_URL,
   POPULAR_BASE_URL,
   API_URL,
-  API_KEY,
+  // API_KEY,
   IMAGE_BASE_URL,
   BACKDROP_SIZE,
   POSTER_SIZE,
-  REQUEST_TOKEN_URL,
-  LOGIN_URL,
-  SESSION_ID_URL,
+  // REQUEST_TOKEN_URL,
+  // LOGIN_URL,
+  // SESSION_ID_URL,
 };
