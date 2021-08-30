9585c340a0aa9f5ff598ecd86aa7df1ad7ace767
Gamdara <uiop12345654321@gmail.com>
2021-08-30 16:29:34 +0800

    comment

--- api/dbMovie.js
+++ api/dbMovie.js
@@ -1,6 +1,17 @@
 import mongoose from "mongoose"
 import mongoosePaginate from "mongoose-paginate-v2"
+
+// dalam mongodb, ada database, collection, dan document
+//  collection ibaratnya seperti table pada mysql,
+//  sedangkan document seperti record" dalam tabel
+//  data pada document disimpan dalam format sepert JSON, 
+//  per document menyimpan 1 object json
+
+// membuat schema untuk menentukan format isi dari tiap document, seperti membuat struktur tabel di mysql
 const movieSchema = mongoose.Schema({
+    // nama : tipedata
+    // nama bebas 
+    // tipedata ada berbagai macam, bisa dilihat di https://docs.mongodb.com/manual/reference/bson-types/
     adult: Boolean,
     backdrop_path : String,
     genre_ids : [Number],
@@ -16,6 +27,12 @@ const movieSchema = mongoose.Schema({
     vote_average: Number,
     vote_count: Number
 })
+
+// menggunakan plugin mongoosePaginate agar bisa lebih mudah paginasi data (contoh ada 1000 data, bisa dipaginasi menjadi 50 page data, tiap page ada 200 data)
 movieSchema.plugin(mongoosePaginate);
 
+// membuat model, model digunakan untuk membaca dan membuat data pada database
+// mongoose.model(param1, param2)
+// param1 = nama collection,
+// param2 = schema yang sudah dibuat,
 export default mongoose.model('movies', movieSchema);
